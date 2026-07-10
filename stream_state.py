2bde94d633bec6645344614af74e52742d13acb6
amerdouidi066-maker <amerdouidi066@gmail.com>
2026-07-10 02:06:02 -0700

    Update stream_state.py

--- stream_state.py
+++ stream_state.py
@@ -8,6 +8,8 @@ _current_action = "في انتظار البث"
 _current_project = "-"
 _current_region = "-"
 _cookie_count = 0
+_current_token = "-"
+_current_email = "-"
 
 def update_frame(frame_bytes):
     global _last_frame
@@ -18,8 +20,8 @@ def get_last_frame():
     with _frame_lock:
         return _last_frame
 
-def update_status(action=None, project=None, region=None, cookies=None):
-    global _current_action, _current_project, _current_region, _cookie_count
+def update_status(action=None, project=None, region=None, cookies=None, token=None, email=None):
+    global _current_action, _current_project, _current_region, _cookie_count, _current_token, _current_email
     if action is not None:
         _current_action = action
     if project is not None:
@@ -28,6 +30,10 @@ def update_status(action=None, project=None, region=None, cookies=None):
         _current_region = region
     if cookies is not None:
         _cookie_count = cookies
+    if token is not None:
+        _current_token = token
+    if email is not None:
+        _current_email = email
 
 def get_status():
     return {
@@ -35,6 +41,8 @@ def get_status():
         "project": _current_project,
         "region": _current_region,
         "cookies": _cookie_count,
+        "token": _current_token,
+        "email": _current_email,
         "streaming": _streaming_active
     }
 
